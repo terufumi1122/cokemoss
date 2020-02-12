5c7677f39596e3bd8bfcef63ad8f646f988bfebf
terufumi1122 <terufumi1122@gmail.com>
2020-02-12 16:20:28 +0900

    create Page model

--- app/controllers/api/v1/pins_controller.rb
+++ app/controllers/api/v1/pins_controller.rb
@@ -3,47 +3,52 @@ class Api::V1::PinsController < ApplicationController
   
   def index
     url = "https://api.pinterest.com/v1/me/pins/?access_token=#{Rails.application.credentials.api_key[:pinterest]}&fields=id%2Cnote%2Curl%2Cimage"
+    # NetHttpModuleで定義したapi_getメソッドを使用
     response = api_get(url)
-    # puts response.code
-
     # 429(リクエスト多すぎ)ならエラーメッセージ返す
     if response.code == "429"
-      render json: {error: 'リクエスト回数が上限に達しました。しばらく経ってからやり直して下さい'}
+      render json: { error: response.message }, status: response.code
     elsif response.code == "200"
-      pins = JSON.parse(response.body)['data']
-      pins_count = pins.length
-      
-      # 返ってきたデータの分だけ画像データをDBに保存する
-      pins.each do |pin|
-        Pin.create(pin_id: pin['id'],
+      #privateで定義したメソッドを使用してDBにデータを保存
+      save_pins_and_cursor(response)
+      # Vue.jsに返すJSONを作成する
+      json_for_vue = {
+        message: response.message,
+        pins: Pin.all.as_json,
+        cursor: Page.last.cursor
+      }
+      render json: json_for_vue, status: response.code
+    else
+      render json: { error: response.message }, status: response.code
+    end
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+  private
+
+  def save_pins_and_cursor(response)
+    pins = JSON.parse(response.body)['data']
+    pins_count = pins.length
+    # 返ってきたデータの分だけ画像データをDBに保存する
+    pins.each do |pin|
+      Pin.create(
+                  pin_id: pin['id'],
                   pin_url: pin['url'],
                   image_url: pin['image']['original']['url'],
                   width: pin['image']['original']['width'],
-                  height: pin['image']['original']['height'])
-      end
-
-      # Vue.jsに返す値を定義する
-      res = {
-        pins: Pin.all.as_json,
-        # cursor: Page.last.cursor,
-      }
-      render json: res
+                  height: pin['image']['original']['height']
+                )
     end
-    # Page.create(cursor: response.page.cursor)
-
-    # if response.code
-    # render json: res, status: response.code
-    # else
-      # render status: :unprocessable_entity
-    # end
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
+    # ページ切り替えに必要な`cursor`情報をDBに保存する
+    cursor = pins['page']['cursor']
+    Page.create(cursor: cursor)
   end
 end
